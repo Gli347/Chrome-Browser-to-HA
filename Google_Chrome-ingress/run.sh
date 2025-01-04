32d62b0365d46ce6104db939e8e118930eb2fe1a
Gli347 <99109479+Gli347@users.noreply.github.com>
2025-01-04 08:29:53 +0100

    Update run.sh

--- Google_Chrome-ingress/run.sh
+++ Google_Chrome-ingress/run.sh
@@ -2,14 +2,14 @@
 set -e
 
 # Start a virtual X server (headless)
-Xvfb :0 -screen 0 1280x800x24 &
-sleep 2
+#Xvfb :0 -screen 0 1280x800x24 &
+#sleep 2
 
 # Optionally launch dbus if you really want the system bus:
 # dbus-daemon --system --fork
 
 # Start Xfce (this calls /usr/bin/X via xinit under the hood)
-#startxfce4 &
+startxfce4 &
 
 # Start x11vnc
 x11vnc -display :0 -nopw -forever -bg
